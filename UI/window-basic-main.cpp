846f04646fd74c163454de6d9d9109f43dc39609
Sung E. Choi <sungeunchoi.kr@gmail.com>
2022-05-25 13:43:09 +0900

    fix: potential sysTrayEnabled config typo fix
    on the original OBS project.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -2085,7 +2085,7 @@ void OBSBasic::OBSInit(std::string configOverrideStr)
 	/* Show the main window, unless the tray icon isn't available
 	 * or neither the setting nor flag for starting minimized is set. */
 	bool sysTrayEnabled = config_get_bool(App()->GlobalConfig(),
-					      "BasicWindow", "sysTrayEnabled");
+					      "BasicWindow", "SysTrayEnabled");
 	bool sysTrayWhenStarted = config_get_bool(
 		App()->GlobalConfig(), "BasicWindow", "SysTrayWhenStarted");
 	bool hideWindowOnStart = QSystemTrayIcon::isSystemTrayAvailable() &&
